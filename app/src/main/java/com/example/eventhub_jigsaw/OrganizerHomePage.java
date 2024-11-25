1297e77b656bff3ba21da867584e17004443fa8d
Rishi Renchen <rishirenchen007@gmail.com>
2024-11-24 17:40:47 -0700

    Organizer stuff started

---
+++ app/src/main/java/com/example/eventhub_jigsaw/OrganizerHomePage.java
@@ -0,0 +1,9 @@
+package com.example.eventhub_jigsaw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class OrganizerHomePage extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+}
