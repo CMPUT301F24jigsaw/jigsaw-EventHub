e5b6b53fe7c2c7431ce6fc162f2ed39d9b51df1a
Tommy <gabeorimoloye@gmail.com>
2024-11-30 07:57:59 -0700

    add admin-view-events feature

--- app/src/main/java/com/example/eventhub_jigsaw/Event.java
+++ app/src/main/java/com/example/eventhub_jigsaw/Event.java
@@ -4,17 +4,27 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents an event in the EventHub app, containing an event name and image.
+ */
 public class Event implements Serializable {
-    private String eventName;
+    private String eventName; // Name of event
     private List<User> attendees;
     private String organizerID;
     private String eventDate;
     private int maxAttendees;
     private String description;
-    private List<User> waitingList;
+    private List<String> waitingList;
     private List<String> sampledUsers;
     private List<String> registeredUsers;
+    private String tempId;
+    /** The image resource ID for the event */
+    private int eventImageResourceUrl; // The image resource Id for the event
 
+    /**
+     * Gets the name of the event.
+     * @return The event name.
+     */
     public String getEventName() {
         return eventName;
     }
@@ -71,6 +81,44 @@ public class Event implements Serializable {
         this.description = description;
     }
 
+    public String getId() {
+        return this.tempId;
+    }
+
+    public void setId(String newId) {
+        if (newId != "") {
+            tempId = newId;
+        }
+    }
+
+    /**
+     * Gets the image resource ID for the event.
+     *
+     * @return The image resource ID associated with the event.
+     */
+    public int getEventImage() {
+        // to be implemented
+        return 1;
+    }
+
+    /**
+     * Constructs a new Events instance with the specified event name and image id
+     *
+     * @param eventName The name of the event.
+     * @param eventDate The date of the event.
+     * @param description The details regarding the event.
+     * @param eventImage The image resource ID associated with the event.
+     */
+    public Event(String eventName, String eventDate, String description) {
+        this.eventName = eventName;
+        this.eventDate = eventDate;
+        this.description = description;
+    }
+
+    public Event() {
+
+    }
+
     public Event(String eventName, String eventDate, String organizerID, int maxAttendees, String description) {
         this.eventName = eventName;
         this.eventDate = eventDate;
@@ -80,11 +128,11 @@ public class Event implements Serializable {
         this.waitingList = new ArrayList<>();
     }
 
-    public List<User> getWaitingList() {
+    public List<String> getWaitingList() {
         return waitingList;
     }
 
-    public void setWaitingList(List<User> waitingList) {
+    public void setWaitingList(List<String> waitingList) {
         this.waitingList = waitingList;
     }
 
@@ -94,12 +142,6 @@ public class Event implements Serializable {
 
     public void setSampledUsers(List<String> sampledUsers) {
         this.sampledUsers = sampledUsers;
-    }
-
-    public Event(String eventName, String eventDate, String description) {
-        this.eventName = eventName;
-        this.eventDate = eventDate;
-        this.description = description;
     }
 }
 
